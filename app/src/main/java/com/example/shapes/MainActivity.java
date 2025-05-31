6cff70d9b0747b9ac325af4a275e8807ff6588a5
AngelVillalobosS <angel.villalobos2269@uppuebla.edu.mx>
2025-05-30 21:41:58 -0600

    Readjustment

--- app/src/main/java/com/example/shapes/MainActivity.java
+++ app/src/main/java/com/example/shapes/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.shapes;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button bCircle, bSquare, bRectangle, bTriangle;
+    ImageButton bCircle, bSquare, bRectangle, bTriangle;
     MediaPlayer circleS, squareS, rectangleS, triangleS;
 
     @Override
@@ -24,7 +27,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bCircle = findViewById(R.id.IB1);
+        bSquare = findViewById(R.id.IB2);
+        bRectangle = findViewById(R.id.IB3);
+        bTriangle = findViewById(R.id.IB4);
+
+        Intent squareView = new Intent(MainActivity.this, squareView.class);
+        startActivity(squareView);
+
+        bSquare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent squareView = new Intent(MainActivity.this, squareView.class);
+                startActivity(squareView);
+            }
+        });
+
+        bRectangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rectangleView = new Intent(MainActivity.this, rectangleView.class);
+                startActivity(rectangleView);
+            }
+        });
+
+        bTriangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent triangleView = new Intent(MainActivity.this, triangleView.class);
+                startActivity(triangleView);
+            }
+        });
+
     }
-
-
 }
